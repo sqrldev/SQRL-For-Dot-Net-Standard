ef7e90cee5a5a1620c8a4e878257f22e738d06af
Liam Raper <liam.raper@pierbridge.com>
2019-10-30 16:26:46 +0000

    Added UI test project with start of frameowrk for tests

---
+++ UITests/LoginTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace UITests
+{
+    [TestClass]
+    public class LoginTests
+    {
+
+        private FirefoxDriver _driver;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _driver = new FirefoxDriver(new FirefoxOptions()
+            {
+                PageLoadStrategy = PageLoadStrategy.Normal
+            });
+        }
+
+        [TestCleanup]
+        public void Shutdown()
+        {
+            _driver.Quit();
+        }
+
+    }
+}
